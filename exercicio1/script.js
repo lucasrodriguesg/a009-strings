9b319ac4db936ee7e122e0d3b8807a265165b615
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-08-31 18:21:43 -0300

    Exercicios do dia 31-08

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+//Resolução 1: 
+const nomeUsuario = prompt ("Digite seu nome:")
+const comidaFavorita1 = prompt ("Digite sua comida favorit: ")
+const comidaFavorita2 = prompt ("Digite sua comida favorit: ")
+const comidaFavorita3 = prompt ("Digite sua comida favorit: ")
+
+console.log("Essas são as comidas preferidas de " + nomeUsuario + ":" , "\nComida 1: " ,comidaFavorita1, "\nComida 2: ",comidaFavorita2, "\nComida 3: ", comidaFavorita3)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
-//Crie a const para a frase aqui
+//Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+
+//Resposta da A
+const fraseCompleta = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+//Resposta da B
+const fraseCompleta2 = fraseCompleta.replace("verde","rosa").replace("azul","laranja")
+console.log(fraseCompleta2)
+
+//Resposta da C
+const novaFrase = fraseCompleta2
+console.log ("Possui a palavra verde? ", novaFrase.includes("verde") , "\nPossui a palavra laranja? ",novaFrase.includes("laranja"))
+
+const fraseMudanca = "BOAS VINDAS, mas não deixe o gato sair"
+const fraseMaiuscula = fraseMudanca.toUpperCase()
+const fraseMinuscula ="Jorge tem uma casa verde e com portão azul, com os dizeres: "
+
+const fraseExtra = fraseMinuscula + fraseMaiuscula 
+console.log (fraseExtra)
